be427542887736e41e2b87d85dcec2011d8a5c8c
darkover21 <darkover21@gmail.com>
2022-08-24 23:39:51 +0200

    Changed time parameter

--- Assets/Scripts/FoodBehaviour.cs
+++ Assets/Scripts/FoodBehaviour.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class FoodBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public BoxCollider2D gridArea;
+
+    private void Start()
     {
-        
+        RandomizePosition();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RandomizePosition()
     {
-        
+        Bounds bounds = gridArea.bounds;
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
     }
 }
+
