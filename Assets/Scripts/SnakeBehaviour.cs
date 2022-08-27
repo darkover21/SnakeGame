e7835ec697475f9d24ff7ee0c15e18ba0cb2d1f4
darkover21 <darkover21@gmail.com>
2022-08-27 10:04:05 +0200

    segments instantiated and moved when food catched

--- Assets/Scripts/SnakeBehaviour.cs
+++ Assets/Scripts/SnakeBehaviour.cs
@@ -8,6 +8,9 @@ public class SnakeBehaviour : MonoBehaviour
 
     private List<Transform> _segments;
 
+    public Transform segmentPrefab;
+    public int initialSize = 4;
+
     private void Start()
     {
         _segments = new List<Transform>();
@@ -36,10 +39,42 @@ public class SnakeBehaviour : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for (int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        
+        }
+
         this.transform.position = new Vector3(
             this.transform.position.x + _direction.x,
             this.transform.position.y + _direction.y,
             0.0f
              );
+    }
+
+    private void Grow() 
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+
+    private void ResetState() 
+    {
+        for (int i = 1; i < _segments.Count; i++) 
+        {
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(this.transform);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food") Grow();
+        else if (other.tag == "Obstacle") ResetState();
+        
     }
 }
